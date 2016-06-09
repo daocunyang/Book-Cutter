763c03b14b77d6e3abafd0fb6fa97e2852093b84
Daocun Yang <ydcdar@gmail.com>
2016-06-09 19:17:45 +0800

    modified txtCut.bat

--- txtCut.bat
+++ txtCut.bat
@@ -1,8 +1,8 @@
 @echo off
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_91
-set classpath=.;%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\lib\tools.jar;要引用的包的路径
+set classpath=.;%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\lib\tools.jar;
 set path=%JAVA_HOME%\bin
 javac Main.java
 java Main
-echo 执行成功...
+echo Success...
 pause
